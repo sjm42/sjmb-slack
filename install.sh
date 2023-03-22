27e62da83a12adf715163bc45ea03b57bafd2d2a
Sami J. Mäkinen <sjm@iki.fi>
2023-03-22 15:04:36 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -x
+set -e
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH
+
+tgt=$HOME/sjmb/bin
+
+rsync -var target/release/sjmb_slack $tgt/
+
+exit 0
+# EOF
